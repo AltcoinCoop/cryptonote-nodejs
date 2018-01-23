f6461fa25bdfe9a933ef2d73b5bd9cdf40213fe6
Spiros Pilafas <spilafas@gmail.com>
2018-01-23 15:13:36 +0200

    Add function for module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const addon = require('./build/Release/cryptonotejs');
+const cryptonote = require('./build/Release/cryptonotejs');
 
-const input = new Buffer("e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f", 'hex');
-rs = addon.cryptonote_hash(input);
+exports.cryptonote_hash = function(input) {
+ 	const buf = new Buffer(input, 'hex');
+ 	rs = cryptonote.cryptonote_hash(buf);
+ 	return rs
+}
 
- console.log("World:",rs);  
 
 
 
